f6328a3a920f962604e341dedf0a36881c209379
Carlos Rafael GC <carlosrafaelgc@gmail.com>
2021-04-20 03:31:14 -0500

    Add files via upload
    Adding completed file.

--- VBA-Challenge .vbs.vb
+++ VBA-Challenge .vbs.vb
@@ -1,72 +1,92 @@
-Sub Loop_Stock_Ticker()
- 
- 'Name my variables
+'Create Subroutine
+Sub StockVolume()
 
 
-Dim Ticker as String
+'Assign variables
 
-Dim PriceChange As Double
- PriceChange = 0
-
-Dim YearOpen As Double
-YearOpen = Cells(2, 3).Value
-
-Dim YearClose As Double
-  YearClose = 0
-
-Dim PercentChange As Double
- PercentChange = 0
- 
-Dim TickerSymbol As Integer
-TickerSymbol = 2
-
+Dim ws As Worksheet
+Dim Ticker As String
 Dim TotalVolume As LongLong
- TotalVolume = 0
+Dim tickersymbol As Integer
+Dim PercentChange As Double
+Dim YearlyChange As Double
+Dim YearlyOpen As Double
+Dim YearlyClose As Double
 
-
-
-    ' create the table for new values
-    Range("i1").Value = "Ticker"
-    Range("j1").Value = "Price_Change"
-    Range("k1").Value = "Percent_Change"
-    Range("l1").Value = "Total_Stock_Volume"
+'Create new Table on all worksheets
+For Each ws In Worksheets
+    TotalVolume = 0
+    tickersymbol = 0
+    
+    ws.Range("I1").Value = "Ticker"
+    ws.Range("J1") = "YearlyChange"
+    ws.Range("K1") = "PercentChange"
+    ws.Range("L1").Value = "Total Stock Volume"
     
     Range("i1").Font.Bold = True
     Range("j1").Font.Bold = True
     Range("k1").Font.Bold = True
     Range("l1").Font.Bold = True
 
-    ' create my loop
- LastRow = Cells(Rows.Count, 1).End(xlUp).Row - 1
+'count to the last row of the columns
+lastrow = Cells(Rows.Count, "A").End(xlUp).Row - 1
 
-    For i = 2 to LastRow
- 
-        If Cells(i + 1, 1).Value <> Cells(i, 1).Value Then
-            Ticker = Cells(i, 1).Value
-
-            TotalVolume = TotalVolume + Cells(i, 7).Value
-
-            YearClose = Cells(i, 6).Value
-
-            Range("I" & TickerSymboL).Value = Cells(i, 1).Value
-    'Range("J")
-    'Range("K")
-    'Range("L" & TotalVolume).Value = TotalVolume
-    'PriceChange = YearClose - YearOpen
-    'PercentChange = (PriceChange / YearOpen) * 100
+'Create loop
+  For i = 2 To lastrow
     
+        If ws.Cells(i + 1, 1).Value <> ws.Cells(i, 1).Value Then
 
-          TickerSymbol = TickerSymbol + 1 
-
-          TotalVolume = TotalVolume + 1
-
-          TotalVolume = 0
-          PriceChange = 0
-          PercentChange = 0
-   
-        End If
+'set ticker
+        Ticker = ws.Cells(i, 1).Value
         
-    Next i 
+'set stock total volume
+         TotalVolume = TotalVolume + ws.Cells(i, 7).Value
+'set Yearly Close
+         YearlyClose = ws.Cells(i, 6).Value
+'set Yearly Open
+         YearlyOpen = ws.Cells(i, 3).Value
+'set Yearly Change
+         YearlyChange = (YearlyClose - YearlyOpen)
+'Add the percent change
+                If YearlyOpen = 0 Then
+            
+                Else
+                PercentChange = (YearlyClose - YearlyOpen) / YearlyOpen
+            
+                End If
+'fill in the data on the new table
+        ws.Range("I" & 2 + tickersymbol).Value = Ticker
+        ws.Range("J" & 2 + tickersymbol).Value = YearlyChange
+        ws.Range("K" & 2 + tickersymbol).Value = PercentChange
+            ws.Columns("K:K").NumberFormat = "0.00%"
+        ws.Range("L" & 2 + tickersymbol).Value = TotalVolume
+'reset Totalvolume
+             TotalVolume = 0
+'Iterate plus 1 down, reset the total for tickersymbol
+             tickersymbol = tickersymbol + 1
+             
+       'format the cells on new table
+        If ws.Range("J" & 2 + tickersymbol).Value >= 0 Then
+        
+            ws.Range("J" & 2 + tickersymbol).Interior.ColorIndex = 4
+            
+        ElseIf ws.Range("J" & 2 + tickersymbol).Value < 0 Then
+        
+            ws.Range("J" & 2 + tickersymbol).Interior.ColorIndex = 3
+            
+        End If
+             
+    Else
+            TotalVolume = TotalVolume + ws.Cells(i, 7).Value
+            
+    End If
 
 
+Next i
+        'reset my count
+        TotalVolume = 0
+        tickersymbol = 0
+
+Next ws
+               
 End Sub
